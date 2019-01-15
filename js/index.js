b277a8ae81a99746c3b47feb2f76950dba1a0148
IMH51 <ianmartinharrison@outlook.com>
2019-01-15 12:27:40 +0000

    completed js oo bing call apply readme

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // use this to copy code snippets or use your browser's console
+
+function.bind(variable)
+
+function.call(variable, argument1 argument2)
+
+function.apply(variable, [argument1, argument2])
